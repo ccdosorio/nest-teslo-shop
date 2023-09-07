f45d5f6e8efcad0fa7f39a230a63a369210f8c8f
Christian Osorio <ccdosorio11@gmail.com>
2023-09-06 22:49:56 -0600

    Fin section 13: Authenticacion y Authorizacion

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,79 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Headers,
+  SetMetadata,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { IncomingHttpHeaders } from 'http';
+
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginUserDto } from './dto';
+import { Auth, GetRawHeaders, GetUser, RoleProtected } from './decorators';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @GetRawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
+    return {
+      ok: true,
+      message: 'Hola mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers,
+    };
+  }
+
+  @Get('private2')
+  // @SetMetadata(META_ROLES, ['admin', 'super-user'])
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get('private3')
+  @Auth()
+  privateRoute3(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+}
---
+++ src/auth/decorators/get-raw-headers.decorator.ts
@@ -0,0 +1,8 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const GetRawHeaders = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const { rawHeaders } = ctx.switchToHttp().getRequest();
+    return rawHeaders;
+  },
+);
